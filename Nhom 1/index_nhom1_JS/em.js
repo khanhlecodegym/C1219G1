36f9cf9b21a3a98ba52d256b2de78c246bad02b6
PhuongHoang <sai@PhuongHoangs-MacBook-Pro.local>
2019-12-10 10:41:31 +0700

    Nhom 1 - Clean up by group

---
+++ Nhom 1/index_nhom1_JS/em.js
@@ -0,0 +1,8 @@
+
+let ten = prompt("Nhap ten");
+let tuoi = prompt("Nhap tuoi");
+let so_thich = prompt("Nhap so thich");
+
+document.getElementById("ten").innerHTML = ten;
+document.getElementById("tuoi").innerHTML = tuoi;
+document.getElementById("so_thich").innerHTML = so_thich;
