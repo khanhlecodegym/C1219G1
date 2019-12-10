25b529f14d251fa8f78221b09c4c38146445914d
TieuNhiKk <hienvh00@gmail.com>
2019-12-10 15:43:40 +0700

    Nhom 1

--- Nhom 1/index_nhom1_JS/em.js
+++
@@ -1,8 +0,0 @@
-
-let ten = prompt("Nhap ten");
-let tuoi = prompt("Nhap tuoi");
-let so_thich = prompt("Nhap so thich");
-
-document.getElementById("ten").innerHTML = ten;
-document.getElementById("tuoi").innerHTML = tuoi;
-document.getElementById("so_thich").innerHTML = so_thich;
